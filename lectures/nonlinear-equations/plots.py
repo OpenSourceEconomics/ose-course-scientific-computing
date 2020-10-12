51fe590eaa4efeb138bc4dea77f578061fe09950
peisenha <peisenha@protonmail.com>
2020-10-12 10:32:33 +0200

    fix build

--- lectures/nonlinear-equations/plots.py
+++ lectures/nonlinear-equations/plots.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_bisection(f, a, b):
+def plot_bisect_example(f, a, b):
     fig, ax = plt.subplots()
     grid = np.linspace(a, b)
     vf = np.vectorize(f)
