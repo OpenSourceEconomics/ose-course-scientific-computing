9af77b6cda81124185dfc6143f55689bd2a9015d
peisenha <peisenha@protonmail.com>
2020-11-13 12:44:49 +0100

    edits

--- lectures/optimization/optimization_plots.py
+++ lectures/optimization/optimization_plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import cm
 
 
 def plot_golden_search_problem(f):
@@ -7,3 +8,68 @@ def plot_golden_search_problem(f):
     grid = np.linspace(0, 3)
     vf = np.vectorize(f)
     ax.plot(grid, vf(grid))
+
+
+def plot_contour(f, allvecs, legend_path):
+
+    X, Y, Z = _get_grid(f, 2)
+
+    fig = plt.figure()
+    # contour_levels=np.logspace(-0.5,3.5,5,base=10)
+    # CS = plt.contour(X,Y,Z,levels=contour_levels)
+    CS = plt.contour(X, Y, Z)
+    plt.clabel(CS, inline=1, fontsize=10)
+    plt.title("objective function")
+    plt.xlabel("variable $x_1$")
+    plt.ylabel("variable $x_2$")
+    plt.rc("text", usetex=False)
+    plt.rc("font", family="serif")
+    plt.plot(1, 1, "r*", markersize=10, label="minimum")
+    plt.plot(4.5, -1.5, "bx", markersize=10, label="initial guess")
+    plt.plot(
+        np.array(allvecs)[:, 0], np.array(allvecs)[:, 1], "go", markersize=4, label=legend_path,
+    )
+    plt.legend()
+    return plt
+
+
+def _get_grid(f, dimension):
+
+    if dimension == 2:
+
+        # create data to visualize objective function
+        n = 50  # number of discretization points along the x-axis
+        m = 50  # number of discretization points along the x-axis
+        a = -2.0
+        b = 5.0  # extreme points in the x-axis
+        c = -2
+        d = 5.0  # extreme points in the y-axis
+
+        X, Y = np.meshgrid(np.linspace(a, b, n), np.linspace(c, d, m))
+        Z = np.zeros(X.shape)
+
+        argument = np.zeros(2)
+        for i in range(X.shape[0]):
+            for j in range(X.shape[1]):
+                argument[0] = X[i, j]
+                argument[1] = Y[i, j]
+                Z[i][j] = f(argument)
+
+        return X, Y, Z
+
+
+def plot_surf(f, dimension=2):
+
+    if dimension == 2:
+        X, Y, Z = _get_grid(f, dimension)
+
+        fig = plt.figure()
+        ax = fig.gca(projection="3d")
+
+        # Plot the surface.
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm)
+
+        plt.xlabel("variable $x_1$")
+        plt.ylabel("variable $x_2$")
+        fig.colorbar(surf)
+        plt.title("objective function")
--- lectures/optimization/optimization_problems.py
+++ lectures/optimization/optimization_problems.py
@@ -9,3 +9,48 @@ def golden_search_problem(x):
 def get_nelder_mead_problem(x):
     # TODO: in progress of integrated in temfpy
     return sp.optimize.rosen(x)
+
+
+def _get_test_function_gradient(x, ill_conditioned, add_noise):
+    dimension = x.shape[0]
+    quadratic_coeff, noise_coeff = _get_tuning_parameters(ill_conditioned, add_noise, dimension)
+
+    return np.array(
+        np.multiply(quadratic_coeff, np.array(x) - np.ones(np.array(x).size))
+    ) + noise_coeff * 2 * np.pi * np.array(
+        np.sin(2 * np.pi * (np.array(x) - np.ones(np.array(x).size)))
+    )
+
+
+def _get_tuning_parameters(ill_conditioned, add_noise, dimension):
+    np.random.seed(123)
+    conditioning_factor = float(ill_conditioned) * 20
+    noise_coeff = 0.5 * float(add_noise)
+    quadratic_coeff = np.array(np.exp(np.random.random(dimension) * conditioning_factor))
+    quadratic_coeff = quadratic_coeff / np.max(quadratic_coeff)
+
+    return quadratic_coeff, noise_coeff
+
+
+def get_test_function(x, ill_conditioned=True, add_noise=True):
+    def _fval(x):
+        return (
+            0.5
+            * np.sum(
+                np.multiply(quadratic_coeff, np.square(np.array(x) - np.ones(np.array(x).size)))
+            )
+            + dimension * noise_coeff
+            - noise_coeff * np.sum(np.cos(2 * np.pi * (np.array(x) - np.ones(np.array(x).size))))
+        )
+
+    def _fhess(x, quadratic_coeff, noise_coeff):
+        return np.diag(quadratic_coeff) + noise_coeff * 4 * np.square(np.pi) * np.diag(
+            np.cos(2 * np.pi * (np.array(x) - np.ones(np.array(x).size)))
+        )
+
+    x = np.atleast_1d(x)
+
+    dimension = x.shape[0]
+    quadratic_coeff, noise_coeff = _get_tuning_parameters(ill_conditioned, add_noise, dimension)
+    return _fval(x)  # , _get_test_function_gradient(x, ill_conditioned, add_noise), _fhess(x,
+    # quadratic_coeff, noise_coeff)
