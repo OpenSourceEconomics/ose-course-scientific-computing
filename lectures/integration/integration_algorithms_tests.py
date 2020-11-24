f0dfbc77d327a9a4edd9011962b66e90f2a38449
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2020-11-24 07:00:51 +0100

    fix import

--- lectures/integration/integration_algorithms_tests.py
+++ lectures/integration/integration_algorithms_tests.py
@@ -1,5 +1,5 @@
 import numpy as np
-from integration_algorithms import monte_carlo_naive
+from integration_algorithms import monte_carlo_naive_unidimensional
 from integration_algorithms import quadrature_gauss_legendre
 from integration_algorithms import quadrature_simpson
 from integration_algorithms import quadrature_trapezoid
@@ -9,6 +9,6 @@ from scipy.stats import uniform
 def test_1():
     approaches = list()
     approaches += [quadrature_gauss_legendre, quadrature_simpson, quadrature_trapezoid]
-    approaches += [monte_carlo_naive]
+    approaches += [monte_carlo_naive_unidimensional]
     for approach in approaches:
         np.testing.assert_almost_equal(approach(uniform.pdf, 0, 1, 11), 1.0)
