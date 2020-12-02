07130766b62cb7430cd05b1b175b063047c1abfd
Annica Gehlen <ama.gehlen@gmail.com>
2020-12-02 17:03:39 +0100

    Update secant method plot, edits to plot legends.

--- lectures/nonlinear_equations/nonlinear_plots.py
+++ lectures/nonlinear_equations/nonlinear_plots.py
@@ -127,65 +127,76 @@ def plot_newtons_method():
     for k, (xi, xinext, yi) in enumerate(zip(x, x[1:], y)):
         plt.plot([xi, xi], [1, yi], "--", color="grey")
         plt.plot([xi, xinext], [yi, 0], "r-")
-        plt.plot(xi, yi, "r.", markersize=16, lw=0, label="$x_k$" if k == 0 else "")
+        plt.plot(xi, yi, "r.", markersize=16, lw=0, label="$f(x_k)$" if k == 0 else "")
         plt.plot(
-            xinext, 0, ".", color="orange", markersize=16, label="$x_{k+1}$" if k == 0 else "",
+            xinext, 0, ".", color="orange", markersize=16, label="$x_k$" if k == 0 else "",
         )
         plt.legend(fontsize=14)
 
+        
+        
 def plot_secant_method():
-    """Illustrates the Secant Method which replaces the derivative in Newton’s method with an estimate.
+    """Illustrates the Secant Method which replaces the derivative in Newton’s method with 
+    an estimate.
+    
     The code for this function is taken from the Python CompEcon toolbox by
     Randall Romero-Aguilar [RA20]_ and has been slightly altered to fit the
     style of these lecture matrials.
+    
     References
     ----------
     .. [RA20] Randall Romero-Aguilar. A Python version of Miranda and Fackler’s
     CompEcon toolbox. 2020. URL: https://github.com/randall-romero/CompEcon.
     """
     # Define function for illustration.
-    
+
     def f(x):
-        return x**5 - 3 
-    
+        return x ** 5 - 3
+
     # Set axis limits and get function values.
     xmin, xmax = 1.0, 2.55
-    x0, xstar = xmax - 0.05, 3**(1/5)
+    x0, xstar = xmax - 0.05, 3 ** (1 / 5)
     x_values = np.linspace(xmin, xmax)
-    y_values= f(x_values)
-    
-    # Defining the function 
+    y_values = f(x_values)
+
+    # Defining the function
     # values to illustrate algorithm.
-    
     n = 4
     x = np.zeros(n)
     x[:2] = x0, x0 - 0.25
     y = f(x)
-    for i in range(2,n):
+    for i in range(2, n):
         x[i] = x[i - 1] - y[i - 1] * (x[i - 1] - x[i - 2]) / (y[i - 1] - y[i - 2])
         y[i] = f(x[i])
-    
-    #Set up figure
+
+    # Set up figure
     plt.figure(figsize=[10, 6])
     plt.title("Secant's Method", fontsize=16)
     plt.xlim(xmin, xmax)
 
     ax = plt.gca()
-    ax.set_xticks( x[:4].tolist() + [xstar])
-    ax.set_xticklabels(['$x_0$', '$x_1$', '$x_2$','$x_3$', '$x^*$'])
+    ax.set_xticks(x[:4].tolist() + [xstar])
+    ax.set_xticklabels(["$x_0$", "$x_1$", "$x_2$", "$x_3$", "$x^*$"])
     ax.set_yticks([])
-    
-    #Plot function
-    plt.plot(x_values,y_values, label="Function $f(x)=x^5 - 3$")
-    plt.hlines(0,xmin, xmax, colors='k')
+
+    # Plot function
+    plt.plot(x_values, y_values, label="Function $f(x)=x^5 - 3$")
+    plt.hlines(0, xmin, xmax, colors="k")
     plt.plot(xstar, 0, "*", color="red", markersize=20, lw=0, label="root")
-    
-    for k, (xi,xinext,yi) in enumerate(zip(x,x[1:],y)):
-        plt.plot([xi,xi],[0,yi],'--', color="grey")
-        plt.plot([xi,xinext],[yi, 0],'r-')
-        plt.plot(xi, yi, "r.", markersize=16, lw=0, label="$x_k$" if k == 0 else "")
-        plt.plot(
-            xinext, 0, ".", color="orange", markersize=16, label="$x_{k+1}$" if k == 0 else "",
-        )
+
+    for k, (xi, xinext, yi) in enumerate(zip(x, x[2:], y)):
+        plt.plot([xi, xinext], [yi, 0], "r-")
         plt.legend(fontsize=14)
+    for k, (xi, yi) in enumerate(zip(x, y)):
+        plt.plot(
+            xi,
+            0,
+            ".",
+            color="orange",
+            markersize=16,
+            label="$x_{k}$" if k == 0 else "",
+        )
+        plt.plot([xi, xi], [0.6, yi], "--", color="grey")
+        plt.plot(xi, yi, "r.", markersize=16, label="$f(x_{k})$" if k == 0 else "")
+    plt.legend(fontsize=14)
         
