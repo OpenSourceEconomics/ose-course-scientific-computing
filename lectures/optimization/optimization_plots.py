f57aa6de5653b434990cf0ea9727d55af1e5a232
Carolina Alvarez <74024000+carolinalvarez@users.noreply.github.com>
2021-01-15 17:08:06 +0100

    Add files via upload

--- lectures/optimization/optimization_plots.py
+++ lectures/optimization/optimization_plots.py
@@ -68,19 +68,59 @@ def plot_surf(f):
 
 # Function with strict global maximum, weak local maximum, strict local maximum
 
+def f(t):
+    return np.where((2 < t) & (t < 2.39), -0.15, np.exp(-0.5*t) * np.cos(3*t) * np.cos(t))
+    
+def plot_optima_example():
 
-def plot_optima_maxima():
-    return np.where((2 < t) & (t < 2.39), -0.15, np.exp(-0.5 * t) * np.cos(3 * t) * np.cos(t))
+    x = np.arange(0.0, 5.0, 0.01)
 
+    plt.figure()
+    plt.plot(x, f(x))
+    plt.plot(0.87, -0.37, ".", color="red", markersize=10, lw=0)
+    plt.plot(2.2, -0.16, "_", color="green", markersize=20, mew=5, lw=0, label="root")
+    plt.plot(4, -0.08, ".", color="red", markersize=10, lw=0)
+    plt.xticks([])
+    plt.yticks([])
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    
+    
 
-x = np.arange(0.0, 5.0, 0.01)
+def f(x):
+    return np.cos(4*x) * np.cos(0.7*x) 
 
-plt.figure()
-plt.plot(0.87, -0.37, ".", color="red", markersize=10, lw=0)
-plt.plot(2.2, -0.16, "_", color="green", markersize=20, mew=5, lw=0, label="root")
-plt.plot(4, -0.08, ".", color="red", markersize=10, lw=0)
-plt.xticks([])
-plt.yticks([])
-plt.xlabel("x")
-plt.ylabel("f(x)")
-plt.plot(x, f(x))
+def plot_true_observed_example():
+
+    x = np.arange(0.0, 5.0, 0.01)
+
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
+    ax1.plot(x, f(x), 'black')
+    ax1.plot(0.5, -0.5, ".", color="blue", markersize=12, lw=0)
+    ax1.plot(0.5, -0.5, ".", color="blue", markersize=12, lw=0)
+    ax1.plot(1.75, 0.25, ".", color="blue", markersize=12, lw=0)
+    ax1.plot(1.95, 0.05, ".", color="blue", markersize=12, lw=0)
+    ax1.plot(2.9, -0.25, ".", color="blue", markersize=12, lw=0)
+    ax1.plot(3.8, 0.75, ".", color="blue", markersize=12, lw=0)
+    ax1.plot(4.1, 0.8, ".", color="blue", markersize=12, lw=0)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('f(x)')
+    ax1.set_title('True function')
+
+    ax2.plot(x, f(x), alpha=0)
+    ax2.plot(0.5, -0.5, ".", color="blue", markersize=12, lw=0)
+    ax2.plot(0.5, -0.5, ".", color="blue", markersize=12, lw=0)
+    ax2.plot(1.75, 0.25, ".", color="blue", markersize=12, lw=0)
+    ax2.plot(1.95, 0.05, ".", color="blue", markersize=12, lw=0)
+    ax2.plot(2.9, -0.25, ".", color="blue", markersize=12, lw=0)
+    ax2.plot(3.8, 0.75, ".", color="blue", markersize=12, lw=0)
+    ax2.plot(4.1, 0.8, ".", color="blue", markersize=12, lw=0)
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('f(x)')
+    ax2.set_title('Observed function')
+
+    
