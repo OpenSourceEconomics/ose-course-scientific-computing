9b43d619ab483096dd7082c27d8d78738cc6f4d4
Annica Gehlen <ama.gehlen@gmail.com>
2021-02-22 21:19:09 +0100

    Adjust plots.

--- lectures/integration/integration_plots.py
+++ lectures/integration/integration_plots.py
@@ -106,162 +106,168 @@ def plot_naive_monte_carlo_randomness():
     ax.scatter(grid, yvals)
     ax.set_xlabel("Seed")
     ax.set_ylabel("Error")
- 
-#Integration plots from the lecture
-#bounded interval function
-
-from numpy import cos, pi, linspace, array
-import numpy as np
-import matplotlib.pyplot as plt
-
-def f(x):
-    return 25 - cos(pi*x)*(2*pi*x - pi + 0.5)**2
-
-x_range = array([0, 1])
-a_b = array([0.25, 0.75])
-n = 401
-
-z = linspace(*a_b, n)
-x = linspace(*x_range, n)
-
-fig, ax = plt.subplots(figsize=[8,4])
-ax.fill_between(z, 0, f(z), alpha=0.35, color='lightgreen')
-ax.hlines(0, *x_range, 'k', linewidth=2)
-ax.vlines(a_b, 0, f(a_b), color='tab:red',linestyle='--',linewidth=2)
-ax.plot(x,f(x), linewidth=3)
-ax.set(xlim=x_range, xticks=a_b,
-       ylim=[-5, f(x).max()+2], yticks=[0])
-       
-ax.set_yticklabels(['0'], size=15)
-ax.set_xticklabels(['$a$', '$b$'], size=15)
-ax.set_facecolor("whitesmoke")
-
-ax.annotate(r'$f(x)$', [x_range[1] - 0.1, f(x_range[1])-5], fontsize=13, color='black', va='top')
-ax.annotate(r'$A = \int_a^bf(x)dx$', [a_b.mean(), 10] ,fontsize=18, ha='center');
-
-# trapezoid rule
-
-from numpy import poly1d, linspace
-import matplotlib.pyplot as plt
-!pip install compecon
-from compecon import qnwtrap, demo
-
-n = 1001
-xmin, xmax = -1, 1
-xwid = xmax-xmin
-x = linspace(xmin, xmax, n)
-
-f = poly1d([2.0, -1.0, 0.5, 5.0])
 
 
-def plot_trap(n):
-    xi, wi = qnwtrap(n+1, xmin, xmax)
+def plot_starting_illustration():
+    """
+    Plot example of continuous real-valued function over bounded interval.
 
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.fill_between(xi, f(xi), alpha=0.35, color='lightgreen')
-    ax.plot(x, f(x), linewidth=3, label=r'$f(x)$')
-    ax.plot(xi, f(xi), color='Tab:red', linestyle='--', label=f'$\\tilde{{f}}_{n+1}(x)$')
-    ax.vlines(xi, 0, f(xi),color='Tab:red', linestyle=':')
-    ax.axhline(0,color='k',linewidth=2)
-    ax.set_facecolor("whitesmoke")
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
+    References
+    ----------
+    Miranda, Mario J, and Paul L Fackler. Applied Computational
+    Economics and Finance, MIT Press, 2002.
 
+    """
+    x_range = np.array([0, 1])
+    a_b = np.array([0.25, 0.75])
+    n = 401
 
+    z = np.linspace(*a_b, n)
+    x = np.linspace(*x_range, n)
 
-    xtl = [f'$x_{i}$' for i in range(n+1)]
-    xtl[0] += '=a'
-    xtl[n] += '=b'
-    ax.set(
-        xlim=[xmin-0.1, xmax+0.1],
-        xticks=xi, 
-        xticklabels=xtl,
-        yticks=[0],
-        yticklabels=['0'])
-    ax.legend()
-    return fig
+    def f(x):
+        return 25 - np.cos(np.pi * x) * (2 * np.pi * x - np.pi + 0.5) ** 2
 
-figs = [plot_trap(n) for n in [2, 4, 8]]
-
-
-#Simpsons rule
-
-from numpy import poly1d,polyfit, linspace, array
-import matplotlib.pyplot as plt
-!pip install compecon
-from compecon import qnwsimp, demo
-
-
-n = 1001
-xmin, xmax = -1, 1
-xwid = xmax-xmin
-x = linspace(xmin, xmax, n)
-
-f = poly1d([2.0, -1.0, 0.5, 5.0])
-
-def fitquad(xi):
-    newcoef = polyfit(xi, f(xi), 2)
-    return poly1d(newcoef)
-
-def plot_simp(n):
-    xi, wi = qnwsimp(n+1, xmin, xmax)
-    
-    fig, ax = plt.subplots(figsize=(8, 4))
+    fig, ax = plt.subplots(figsize=[10, 6])
+    ax.fill_between(z, 0, f(z), alpha=0.35, color="gold")
+    ax.hlines(0, *x_range, "k", linewidth=2)
+    ax.vlines(a_b, 0, f(a_b), color="tab:red", linestyle="--", linewidth=2)
     ax.plot(x, f(x), linewidth=3)
-    ax.set_facecolor("whitesmoke")
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    
+    ax.set(xlim=x_range, xticks=a_b, ylim=[-5, f(x).max() + 2], yticks=[0])
 
-    for k in range(n//2):
-        xii = xi[(2*k):(2*k+3)]
-        xiii = linspace(xii[0], xii[2], 125)
-        p = fitquad(xii)
-        ax.fill_between(xiii, p(xiii), alpha=0.35, color='lightgreen')    
-        ax.plot(xiii, p(xiii),color='Tab:red', linestyle='--')
-        fig, ax = plt.subplots(figsize=(7, 3))
-        ax.set_facecolor("whitesmoke")
+    ax.set_yticklabels(["0"], size=15)
+    ax.set_xticklabels(["$a$", "$b$"], size=15)
+
+    ax.annotate(
+        r"$f(x)$",
+        [x_range[1] - 0.1, f(x_range[1]) - 5],
+        fontsize=16,
+        color="black",
+        va="top",
+    )
+    ax.annotate(r"$A = \int_a^bf(x)dx$", [a_b.mean(), 10], fontsize=18, ha="center")
+
+
+def get_trapezoid_quadrature_nodes_and_weights(n, a, b):
+    """
+    Compute univariate trapezoid rule quadrature nodes and weights.
+
+    References
+    ----------
+    Miranda, Mario J, and Paul L Fackler. Applied Computational
+    Economics and Finance, MIT Press, 2002.
+
+    """
+    if n < 1:
+        raise ValueError("n must be at least one")
+
+    nodes = np.linspace(a, b, n)
+    dx = nodes[1] - nodes[0]
+
+    weights = dx * np.ones(n)
+    weights[0] *= 0.5
+    weights[-1] *= 0.5
+
+    return nodes, weights
+
+
+def plot_trapezoid_rule_illustration():
+    """
+    Plot illustrations of trapezoid rule.
+
+    References
+    ----------
+    Miranda, Mario J, and Paul L Fackler. Applied Computational
+    Economics and Finance, MIT Press, 2002.
+
+    """
+    xmin, xmax = -1, 1
+    f = np.poly1d([2.0, -1.0, 0.5, 5.0])
+    x = np.linspace(xmin, xmax, 1001)
+
+    for n in 2, 4, 8:
+
+        xi, wi = get_trapezoid_quadrature_nodes_and_weights(n + 1, xmin, xmax)
+
+        fig, ax = plt.subplots(figsize=[10, 6])
+        ax.fill_between(xi, f(xi), alpha=0.35, color="gold")
+        ax.plot(x, f(x), linewidth=3, label=r"$f(x)$")
+        ax.plot(xi, f(xi), color="Tab:red", linestyle="--", label=f"$\\tilde{{f}}_{n+1}(x)$")
+        ax.vlines(xi, 0, f(xi), color="Tab:red", linestyle=":")
+        ax.axhline(0, color="k", linewidth=2)
         plt.xticks(fontsize=14)
         plt.yticks(fontsize=14)
-    
-    plt.vlines(xi, 0, f(xi),'k', linestyle=':')
-    plt.hlines(0,xmin-0.1, xmax+0.1,'k',linewidth=2)
-    plt.xlim(xmin-0.1, xmax+0.1)
-    xtl = ['$x_{%d}$' % i for i in range(n+1)]
-    xtl[0] += '=a'
-    xtl[n] += '=b'
-    plt.xticks(xi, xtl)
-    plt.yticks([0],['0'])
-    plt.legend([r'$f(x)$', f'$\\tilde{{f}}_{n+1}(x)$'])
-    return fig
 
-def plot_simp(n):
-    xi, wi = qnwsimp(n+1, xmin, xmax)
-    
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(x, f(x), linewidth=3)
-    ax.set_facecolor("whitesmoke")
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    
-    for k in range(n//2):
-        xii = xi[(2*k):(2*k+3)]
-        xiii = linspace(xii[0], xii[2], 125)
-        p = fitquad(xii)
-        ax.fill_between(xiii, p(xiii), alpha=0.35, color='lightgreen')    
-        ax.plot(xiii, p(xiii), color='Tab:red', linestyle='--')
-    
-    ax.vlines(xi, 0, f(xi), color='Tab:red', linestyle=':')
-    ax.axhline(0,color='k',linewidth=2)
-    
-    xtl = [f'$x_{i}$' for i in range(n+1)]
-    xtl[0] += '=a'
-    xtl[n] += '=b'
-    
-    ax.set(xlim=[xmin-0.1, xmax+0.1], xticks=xi, xticklabels=xtl,
-           yticks=[0], yticklabels=['0'])
-    
-    plt.legend([r'$f(x)$', f'$\\tilde{{f}}_{n+1}(x)$'])
-    return fig
+        xtl = [f"$x_{i}$" for i in range(n + 1)]
+        xtl[0] += "=a"
+        xtl[n] += "=b"
+        ax.set(
+            xlim=[xmin - 0.1, xmax + 0.1],
+            xticks=xi,
+            xticklabels=xtl,
+            yticks=[0],
+            yticklabels=["0"],
+        )
+        ax.legend(fontsize=16)
 
-figs = [plot_simp(n) for n in [2, 4, 8]]
+
+def get_simpsons_quadrature_nodes_and_weights(n, a, b):
+    """
+    Compute univariate Simpson quadrature nodes and weights.
+
+    References
+    ----------
+    Miranda, Mario J, and Paul L Fackler. Applied Computational
+    Economics and Finance, MIT Press, 2002.
+
+    """
+    nodes = np.linspace(a, b, n)
+    dx = nodes[1] - nodes[0]
+    weights = np.tile([2.0, 4.0], int((n + 1) / 2))
+    weights = weights[:n]
+    weights[0] = weights[-1] = 1
+    weights = (dx / 3.0) * weights
+
+    return nodes, weights
+
+
+def plot_simpsons_rule_illustration():
+    """
+    Plot illustrations of Simpson's rule.
+
+    References
+    ----------
+    Miranda, Mario J, and Paul L Fackler. Applied Computational
+    Economics and Finance, MIT Press, 2002.
+
+    """
+    xmin, xmax = -1, 1
+    f = np.poly1d([2.0, -1.0, 0.5, 5.0])
+    x = np.linspace(xmin, xmax, 1001)
+
+    def fitquad(xi):
+        newcoef = np.polyfit(xi, f(xi), 2)
+        return np.poly1d(newcoef)
+
+    for n in [2, 4, 8]:
+        xi, wi = get_simpsons_quadrature_nodes_and_weights(n + 1, xmin, xmax)
+
+        fig, ax = plt.subplots(figsize=[10, 6])
+        ax.plot(x, f(x), linewidth=3)
+
+        for k in range(n // 2):
+            xii = xi[(2 * k) : (2 * k + 3)]
+            xiii = np.linspace(xii[0], xii[2], 125)
+            p = fitquad(xii)
+            ax.fill_between(xiii, p(xiii), alpha=0.35, color="gold")
+            ax.plot(xiii, p(xiii), color="tab:red", linestyle="--")
+
+        plt.vlines(xi, 0, f(xi), "k", linestyle=":")
+        plt.hlines(0, xmin - 0.1, xmax + 0.1, "k", linewidth=2)
+        plt.xlim(xmin - 0.1, xmax + 0.1)
+        xtl = ["$x_{%d}$" % i for i in range(n + 1)]
+        xtl[0] += "=a"
+        xtl[n] += "=b"
+        plt.xticks(xi, xtl, fontsize=14)
+        plt.yticks([0], ["0"], fontsize=14)
+        plt.legend([r"$f(x)$", f"$\\tilde{{f}}_{n+1}(x)$"], fontsize=16)
