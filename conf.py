e2a4b0afc026f85fbf15fe46b1eca57c2d0b3975
s6emschw <64075224+s6emschw@users.noreply.github.com>
2021-12-02 16:14:13 +0100

    Changed path.

--- conf.py
+++ conf.py
@@ -11,7 +11,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("labs/linear_equations"))
 print(sys.executable)
 
 # -- Project information -----------------------------------------------------
